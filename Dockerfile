5d112c39cc7ab0a86ac22b60e34d7677e8871225
Oliver Kraemer <oliverkra@gmail.com>
2019-07-08 22:36:14 -0300

    Simplified server

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM golang:1.9-alpine
+FROM golang:alpine
 RUN apk add --no-cache git
 WORKDIR /usr/src/app
 COPY . /usr/src/app
-RUN go get github.com/go-martini/martini && go build -o meuip
+RUN go get github.com/labstack/echo && go build -o meuip
 
-FROM alpine:3.6
+FROM alpine:latest
 COPY --from=0 /usr/src/app/meuip /usr/local/bin/meuip
 
 EXPOSE 3000
--- main.go
+++ main.go
@@ -1,100 +1,54 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"net"
 	"net/http"
 	"strings"
 
-	"github.com/go-martini/martini"
+	"github.com/labstack/echo"
 )
 
-type Register struct {
-	Name   string
-	Key    string
-	LastIP string
-}
-
-var registered = make(map[string]*Register, 0)
-
 func main() {
-	m := martini.Classic()
+	addr := flag.String("addr", ":3000", "Listen address")
+	flag.Parse()
 
-	m.Get("/", GetMyIP)
-	m.Group("/(?P<name>[a-zA-Z]{3,})", func(r martini.Router) {
-		m.Get("", GetRegisteredIP)
-		m.Post("/:key", RegisterMyIP)
-		m.Delete("/:key", DeleteMyIP)
-	})
+	e := echo.New()
+	e.GET("/", ipHandler)
+	e.GET("/debug", debugHandler)
 
-	m.Run()
-}
-
-func GetMyIP(r *http.Request) string {
-	return RealIPFromRequest(r)
-}
-
-func GetRegisteredIP(r *http.Request, p martini.Params) (int, string) {
-	name := p["name"]
-
-	if r, exists := registered[name]; exists {
-		return http.StatusOK, r.LastIP
-	}
-
-	return http.StatusNotFound, "Not found"
-}
-
-func RegisterMyIP(r *http.Request, p martini.Params) (int, string) {
-	name := p["name"]
-	key := p["key"]
-
-	reg, exists := registered[name]
-	if exists && reg.Key != key {
-		return http.StatusBadRequest, "Bad key"
-	}
-
-	if exists {
-		reg.LastIP = RealIPFromRequest(r)
-	} else {
-		registered[name] = NewRegister(name, key, RealIPFromRequest(r))
-	}
-
-	return http.StatusOK, "Success!"
-}
-
-func DeleteMyIP(r *http.Request, p martini.Params) (int, string) {
-	name := p["name"]
-	key := p["key"]
-
-	reg, exists := registered[name]
-	if exists && reg.Key != key {
-		return http.StatusBadRequest, "Bad key"
-	}
-
-	if exists {
-		delete(registered, name)
-	}
-
-	return http.StatusOK, "Success!"
-}
-
-func NewRegister(name, key, lastip string) *Register {
-	return &Register{
-		Name:   name,
-		Key:    key,
-		LastIP: lastip,
+	if err := e.Start(*addr); err != nil {
+		panic(err)
 	}
 }
 
-const HeaderXForwardedFor = "X-Forwarded-For"
-const HeaderXRealIP = "X-Real-IP"
+func ipHandler(ctx echo.Context) error {
+	return ctx.String(http.StatusOK, realIPFromRequest(ctx.Request()))
+}
 
-func RealIPFromRequest(r *http.Request) string {
-	if ip := r.Header.Get(HeaderXForwardedFor); ip != "" {
+func debugHandler(ctx echo.Context) error {
+	h := ctx.Request().Header
+	return ctx.String(http.StatusOK, pretty(h))
+}
+
+func realIPFromRequest(r *http.Request) string {
+	const (
+		headerXForwardedFor = "X-Forwarded-For"
+		headerXRealIP       = "X-Real-IP"
+	)
+
+	if ip := r.Header.Get(headerXForwardedFor); ip != "" {
 		return strings.Split(ip, ", ")[0]
 	}
-	if ip := r.Header.Get(HeaderXRealIP); ip != "" {
+	if ip := r.Header.Get(headerXRealIP); ip != "" {
 		return ip
 	}
 	ra, _, _ := net.SplitHostPort(r.RemoteAddr)
 	return ra
+}
+
+func pretty(v interface{}) string {
+	b, _ := json.MarshalIndent(v, "", "  ")
+	return string(b)
 }
